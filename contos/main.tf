3190700a35f716b50c3107976aee94f7c785ca3d
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-08-14 22:05:37 +0530

    Update main.tf

--- contos/main.tf
+++ contos/main.tf
@@ -1,1 +1,8 @@
-
+resource "azurerm_management_group" "managementgroup_parent" {
+  display_name = var.managementgroup
+/* If you wish to use Subscription ID Mapping with Management Groups
+  subscription_ids = [
+    data.azurerm_subscription.current.subscription_id,
+  ] 
+*/
+}
