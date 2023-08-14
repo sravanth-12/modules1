677b5aede2d06b17406eba3cf2dc055039ebfbdb
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-08-14 22:15:25 +0530

    Update outputs.tf

--- contos/platform/connectivity/network/outputs.tf
+++ contos/platform/connectivity/network/outputs.tf
@@ -1,1 +1,12 @@
-
+/*output "resource_group_name" {
+  value = azurerm_resource_group.resource-group.name
+}
+output "resource_group_location" {
+    value =  azurerm_resource_group.resource-group.location 
+}*/
+output "vnet" {
+  value = azurerm_virtual_network.vnet
+}
+output "subnet" {
+  value = azurerm_subnet.vnet-subnet
+}
