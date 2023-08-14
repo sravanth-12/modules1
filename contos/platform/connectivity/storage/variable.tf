2de7331c83b41f7c196e2695b3d9be80402bf122
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-08-14 22:19:36 +0530

    Update variable.tf

--- contos/platform/connectivity/storage/variable.tf
+++ contos/platform/connectivity/storage/variable.tf
@@ -1,1 +1,18 @@
-
+variable "primary_database" {
+    type = string
+}
+variable "resource_group" {
+    type = string
+}
+variable "location" {
+    type = string
+}
+variable "primary_database_version" {
+    type = string
+}
+variable "primary_database_admin" {
+    type = string
+}
+variable "primary_database_password" {
+    type = string
+}
