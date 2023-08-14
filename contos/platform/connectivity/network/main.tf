f40fc6ab55e4c3822df410ea3b7f94c415c6a910
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-08-14 22:14:54 +0530

    Update main.tf

--- contos/platform/connectivity/network/main.tf
+++ contos/platform/connectivity/network/main.tf
@@ -1,1 +1,13 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet-name
+  address_space       = var.vnet-address-space
+  location            = var.location
+  resource_group_name = var.resouce-group-name
+} 
 
+resource "azurerm_subnet" "vnet-subnet" {
+  name                 = var.subnet-name
+  resource_group_name  = var.resouce-group-name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet-address-prefix
+}
