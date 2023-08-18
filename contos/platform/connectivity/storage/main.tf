a7acfd5431b6e0bb0914af11086a955a5507b448
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-08-18 17:21:44 +0530

    Update main.tf

--- contos/platform/connectivity/storage/main.tf
+++ contos/platform/connectivity/storage/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_mssql_server" "primary_database" {
     name = var.primary_database
-    resource_group_name = var.resource_group
+    resource_group_name = var.resource-group-name
     location = var.location
     version = var.primary_database_version
     administrator_login = var.primary_database_admin
@@ -8,7 +8,7 @@ resource "azurerm_mssql_server" "primary_database" {
 }
 resource "azurerm_sql_database" "db" {
   name                = "db"
-  resource_group_name = var.resource_group
+  resource_group_name = var.resource-group-name
   location            = var.location
   server_name         = azurerm_mssql_server.primary_database.name
 }
