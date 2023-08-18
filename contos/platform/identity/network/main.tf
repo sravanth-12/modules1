b75c0504f2d1325ea6cf73e454e8ee773e0f0206
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-08-18 10:42:19 +0530

    Update main.tf

--- contos/platform/identity/network/main.tf
+++ contos/platform/identity/network/main.tf
@@ -1,1 +1,13 @@
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet-name
+  address_space       = var.vnet_address_space
+  location           = var.location1
+  resource_group_name = var.resource-group1-name
+}
+resource "azurerm_subnet" "subnet" {
+  name = var.subnet-name
+  resource_group_name  =var.resource-group1-name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet-address_prefixes
+}
