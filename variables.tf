4ab21488388cca93c53831d31ddc6a0f5cfdda63
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-08-19 15:04:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -306,20 +306,25 @@ variable "db-nsg-sr2-destination_address_prefix" {
 variable "db-nsg-sr2-destination_port_range" {
   type = string
 }
-variable "primary_database" {
-    type = string
-}
+
+
 variable "resource-group1-name" {
     type = string
 }
 variable "location1" {
     type = string
 }
+
+
 variable "vnet_address_space" {
     type = list(string)
 }
 variable "subnet-address_prefixes" {
     type = list(string)
+}
+
+variable "primary_database" {
+    type = string
 }
 variable "primary_database_version" {
     type = string
@@ -329,4 +334,248 @@ variable "primary_database_admin" {
 }
 variable "primary_database_password" {
     type = string
+}
+
+
+variable "NIC_name" {
+    type = string
+    description = "Name of the Network Interface Card"
+}
+
+variable "ip_configuration_name" {
+    type = string
+    description = "Private IP configuration name"
+}
+
+variable "IP_allocation" {
+    type = string
+    description = "Allocation method of IP address for virtual machine"
+}
+
+variable "encryption_algorithm" {
+    type = string
+    description = "Public Key encryption algorithm"
+}
+
+variable "rsa_bits" {
+    type = number
+    description = "Number of RSA bits"
+}
+
+variable "vm_name" {
+    type = string
+    description = "Name of virtual machine"
+}
+
+variable "size" {
+    type = string
+    description = "Disk size and CPU of virtual machine"
+}
+
+variable "username" {
+    type = string
+    description = "Admin username for virtual machine"
+}
+
+variable "OS_disk_caching" {
+    type = string
+    description = "Caching type of OS disk"
+}
+
+variable "stgacc_type" {
+    type = string
+    description = "Storage account type of OS disk in virtual machine"
+}
+
+variable "publisher" {
+    type = string
+    description = "Publisher name of virtual machine source image"
+}
+
+variable "offer" {
+    type = string
+    description = "virtual machine source image OS"
+}
+
+variable "vm_sku" {
+    type = string
+    description = "SKU of virtual machine OS"
+}
+
+variable "OS_version" {
+    type = string
+    description = "OS version of virtual machine"
+}
+
+
+
+variable "web-nsg-sr1-name" {
+  type = string
+}
+variable "web-nsg-sr1-priority" {
+  type = string
+}
+variable "web-nsg-sr1-direction" {
+  type = string
+}
+variable "web-nsg-sr1-access" {
+  type = string
+}
+variable "web-nsg-sr1-protocol" {
+  type = string
+}
+variable "web-nsg-sr1-source_address_prefix" {
+  type = string
+}
+variable "web-nsg-sr1-source_port_range" {
+  type = string
+}
+variable "web-nsg-sr1-destination_address_prefix" {
+  type = string
+} 
+variable "web-nsg-sr1-destination_port_range" {
+  type = string
+}
+variable "web-nsg-sr2-name" {
+  type = string
+}
+  variable "web-nsg-sr2-priority" {
+  type = string
+}
+variable "web-nsg-sr2-direction" {
+  type = string
+}
+variable "web-nsg-sr2-access" {
+  type = string
+}
+variable "web-nsg-sr2-protocol" {
+  type = string
+}
+variable "web-nsg-sr2-source_address_prefix" {
+  type = string
+}
+variable "web-nsg-sr2-source_port_range" {
+  type = string
+}
+variable "web-nsg-sr2-destination_address_prefix" {
+  type = string
+} 
+variable "web-nsg-sr2-destination_port_range" {
+  type = string
+}
+
+variable "web_subnet_id" {
+  type = string
+}
+variable "app-nsg-sr1-name" {
+  type =string
+}
+variable "app-nsg-sr1-priority" {
+  type = string
+}
+variable "app-nsg-sr1-protocol" {
+  type = string
+}
+variable "app-nsg-sr1-access" {
+  type = string
+}
+variable "app-nsg-sr1-direction" {
+  type = string
+}
+variable "app-nsg-sr1-destination_address_prefix" {
+  type = string
+}
+variable "app-nsg-sr1-destination_port_range" {
+  type = string
+}
+variable "app-nsg-sr1-source_address_prefix" {
+  type = string
+}
+variable "app-nsg-sr1-source_port_range" {
+  type = string
+}
+variable "app-nsg-sr2-name" {
+  type =string
+}
+variable "app-nsg-sr2-priority" {
+  type = string
+}
+variable "app-nsg-sr2-protocol" {
+  type = string
+}
+variable "app-nsg-sr2-access" {
+  type = string
+}
+variable "app-nsg-sr2-direction" {
+  type = string
+}
+variable "app-nsg-sr2-destination_address_prefix" {
+  type = string
+}
+variable "app-nsg-sr2-destination_port_range" {
+  type = string
+}
+variable "app-nsg-sr2-source_address_prefix" {
+  type = string
+}
+variable "app-nsg-sr2-source_port_range" {
+  type = string
+}
+/*//*/
+variable "app_subnet_id" {
+  type = string
+}
+variable "db-nsg-sr1-name" {
+  type =string  
+}
+variable "db-nsg-sr1-priority" {
+  type = string
+}
+variable "db-nsg-sr1-protocol" {
+  type = string
+}
+variable "db-nsg-sr1-access" {
+  type = string
+}
+variable "db-nsg-sr1-direction" {
+  type = string
+}
+variable "db-nsg-sr1-source_address_prefix" {
+  type = string
+}
+variable "db-nsg-sr1-source_port_range" {
+  type = string
+}
+variable "db-nsg-sr1-destination_address_prefix" {
+  type = string
+}
+variable "db-nsg-sr1-destination_port_range" {
+  type = string
+}/*//*/
+variable "db-nsg-sr2-name" {
+  type =string  
+}
+variable "db-nsg-sr2-priority" {
+  type = string
+}
+variable "db-nsg-sr2-protocol" {
+  type = string
+}
+variable "db-nsg-sr2-access" {
+  type = string
+}
+variable "db-nsg-sr2-direction" {
+  type = string
+}
+variable "db-nsg-sr2-source_address_prefix" {
+  type = string
+}
+variable "db-nsg-sr2-source_port_range" {
+  type = string
+}
+variable "db-nsg-sr2-destination_address_prefix" {
+  type = string
+}
+variable "db-nsg-sr2-destination_port_range" {
+  type = string
 }
