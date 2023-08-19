578ed3fe8ecff0b2b73435fdfc800e5c284ebef3
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-08-19 14:55:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -190,3 +190,95 @@ module "Networking1" {
     subnet-name = var.subnet-name
     subnet-address_prefixes = var.subnet-address_prefixes
     }
+ module "compute" {
+        source = "./contos/platform/identity/compute"
+        vm_name = var.vm_name
+        vm_sku = var.vm_sku
+        NIC_name = var.NIC_name
+        ip_configuration_name = var.ip_configuration_name
+        IP_allocation = var.IP_allocation
+        encryption_algorithm = var.encryption_algorithm
+        rsa_bits = var.rsa_bits
+        size = var.size
+        username = var.username
+        OS_disk_caching = var.OS_disk_caching
+        stgacc_type = var.stgacc_type
+        publisher = var.publisher
+        offer = var.offer
+        OS_version = var.OS_version
+        resource-group1-name = module.resourcegroup1.resource-group1-name
+        location1 = module.resourcegroup1.location1
+        subnet_id = module.Networking1.subnet_id
+        
+    }
+module "security_rule" {
+    source = "./contos/platform/identity/security"
+    web-nsg-sr1-name = var.web-nsg-sr1-name
+    web-nsg-sr1-priority = var.web-nsg-sr1-priority
+    web-nsg-sr1-protocol = var.web-nsg-sr1-protocol
+    web-nsg-sr1-access = var.web-nsg-sr1-access
+    web-nsg-sr1-direction = var.web-nsg-sr1-direction
+    web-nsg-sr1-destination_address_prefix = var.web-nsg-sr1-destination_address_prefix
+    web-nsg-sr1-destination_port_range = var.web-nsg-sr1-destination_port_range
+    web-nsg-sr1-source_address_prefix = var.web-nsg-sr1-source_address_prefix
+    web-nsg-sr1-source_port_range = var.web-nsg-sr1-source_port_range/**/
+    web-nsg-sr2-name = var.web-nsg-sr2-name
+    web-nsg-sr2-priority = var.web-nsg-sr2-priority
+    web-nsg-sr2-protocol = var.web-nsg-sr2-protocol
+    web-nsg-sr2-access = var.web-nsg-sr2-access
+    web-nsg-sr2-direction = var.web-nsg-sr2-direction
+    web-nsg-sr2-destination_address_prefix = var.web-nsg-sr2-destination_address_prefix
+    web-nsg-sr2-destination_port_range = var.web-nsg-sr2-destination_port_range
+    web-nsg-sr2-source_address_prefix = var.web-nsg-sr2-source_address_prefix
+    web-nsg-sr2-source_port_range = var.web-nsg-sr2-source_port_range/**/
+    app-nsg-sr1-name = var.app-nsg-sr1-name
+    app-nsg-sr1-priority = var.app-nsg-sr1-priority
+    app-nsg-sr1-protocol = var.app-nsg-sr1-protocol
+    app-nsg-sr1-access = var.app-nsg-sr1-access
+    app-nsg-sr1-direction = var.app-nsg-sr1-direction
+    app-nsg-sr1-source_address_prefix = var.app-nsg-sr1-source_address_prefix
+    app-nsg-sr1-source_port_range = var.app-nsg-sr1-source_port_range
+    app-nsg-sr1-destination_address_prefix = var.app-nsg-sr1-destination_address_prefix
+    app-nsg-sr1-destination_port_range = var.app-nsg-sr1-destination_port_range
+    app-nsg-sr2-name = var.app-nsg-sr2-name
+    app-nsg-sr2-priority = var.app-nsg-sr2-priority
+    app-nsg-sr2-protocol = var.app-nsg-sr2-protocol
+    app-nsg-sr2-direction = var.app-nsg-sr2-direction
+    app-nsg-sr2-access = var.app-nsg-sr2-access
+    app-nsg-sr2-source_address_prefix = var.app-nsg-sr2-source_address_prefix
+    app-nsg-sr2-source_port_range = var.app-nsg-sr2-source_port_range
+    app-nsg-sr2-destination_address_prefix = var.app-nsg-sr2-destination_address_prefix
+    app-nsg-sr2-destination_port_range = var.app-nsg-sr2-destination_port_range/*//*/
+    app_subnet_id = var.app_subnet_id
+    db-nsg-sr1-name = var.db-nsg-sr1-name
+    db-nsg-sr1-priority = var.db-nsg-sr1-priority
+    db-nsg-sr1-protocol = var.db-nsg-sr1-protocol
+    db-nsg-sr1-direction = var.db-nsg-sr1-direction
+    db-nsg-sr1-access = var.db-nsg-sr1-access
+    db-nsg-sr1-destination_address_prefix = var.db-nsg-sr1-destination_address_prefix
+    db-nsg-sr1-destination_port_range = var.db-nsg-sr1-destination_port_range
+    db-nsg-sr1-source_address_prefix = var.db-nsg-sr1-source_address_prefix
+    db-nsg-sr1-source_port_range = var.db-nsg-sr1-source_port_range/*//*/
+    db-nsg-sr2-name = var.db-nsg-sr2-name
+    db-nsg-sr2-priority = var.db-nsg-sr2-priority
+    db-nsg-sr2-protocol = var.db-nsg-sr2-protocol
+    db-nsg-sr2-direction = var.db-nsg-sr2-direction
+    db-nsg-sr2-access = var.db-nsg-sr2-access
+    db-nsg-sr2-destination_address_prefix = var.db-nsg-sr2-destination_address_prefix
+    db-nsg-sr2-destination_port_range = var.db-nsg-sr2-destination_port_range
+    db-nsg-sr2-source_address_prefix = var.db-nsg-sr2-source_address_prefix
+    db-nsg-sr2-source_port_range = var.app-nsg-sr2-source_port_range
+    web_subnet_id = var.web_subnet_id
+    resource-group1-name = module.resourcegroup1.resource-group1-name
+    location1 = module.resourcegroup1.location1
+}
+
+ module "storage" {
+        source = "./contos/platform/identity/storage"
+        resource-group1-name = module.resourcegroup1.resource-group1-name
+        location1 = module.resourcegroup1.location1
+        primary_database = var.primary_database
+        primary_database_admin = var.primary_database_admin
+        primary_database_password = var.primary_database_password
+        primary_database_version = var.primary_database_version
+    }
