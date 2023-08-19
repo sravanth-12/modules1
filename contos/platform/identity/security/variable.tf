1a47665946824554a0f0a5ac634386bed04d5960
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-08-19 15:09:24 +0530

    Create variable.tf

---
+++ contos/platform/identity/security/variable.tf
@@ -0,0 +1,176 @@
+variable "location1" {
+    type = string
+}
+variable "resource-group1-name" {
+    type = string
+}
+variable "web-nsg-sr1-name" {
+  type = string
+}
+variable "web-nsg-sr1-priority" {
+  type = string
+}
+variable "web-nsg-sr1-direction" {
+  type = string
+}
+variable "web-nsg-sr1-access" {
+  type = string
+}
+variable "web-nsg-sr1-protocol" {
+  type = string
+}
+variable "web-nsg-sr1-source_address_prefix" {
+  type = string
+}
+variable "web-nsg-sr1-source_port_range" {
+  type = string
+}
+variable "web-nsg-sr1-destination_address_prefix" {
+  type = string
+} 
+variable "web-nsg-sr1-destination_port_range" {
+  type = string
+}
+variable "web-nsg-sr2-name" {
+  type = string
+}
+  variable "web-nsg-sr2-priority" {
+  type = string
+}
+variable "web-nsg-sr2-direction" {
+  type = string
+}
+variable "web-nsg-sr2-access" {
+  type = string
+}
+variable "web-nsg-sr2-protocol" {
+  type = string
+}
+variable "web-nsg-sr2-source_address_prefix" {
+  type = string
+}
+variable "web-nsg-sr2-source_port_range" {
+  type = string
+}
+variable "web-nsg-sr2-destination_address_prefix" {
+  type = string
+} 
+variable "web-nsg-sr2-destination_port_range" {
+  type = string
+}
+
+variable "web_subnet_id" {
+  type = string
+}
+variable "app-nsg-sr1-name" {
+  type =string
+}
+variable "app-nsg-sr1-priority" {
+  type = string
+}
+variable "app-nsg-sr1-protocol" {
+  type = string
+}
+variable "app-nsg-sr1-access" {
+  type = string
+}
+variable "app-nsg-sr1-direction" {
+  type = string
+}
+variable "app-nsg-sr1-destination_address_prefix" {
+  type = string
+}
+variable "app-nsg-sr1-destination_port_range" {
+  type = string
+}
+variable "app-nsg-sr1-source_address_prefix" {
+  type = string
+}
+variable "app-nsg-sr1-source_port_range" {
+  type = string
+}
+variable "app-nsg-sr2-name" {
+  type =string
+}
+variable "app-nsg-sr2-priority" {
+  type = string
+}
+variable "app-nsg-sr2-protocol" {
+  type = string
+}
+variable "app-nsg-sr2-access" {
+  type = string
+}
+variable "app-nsg-sr2-direction" {
+  type = string
+}
+variable "app-nsg-sr2-destination_address_prefix" {
+  type = string
+}
+variable "app-nsg-sr2-destination_port_range" {
+  type = string
+}
+variable "app-nsg-sr2-source_address_prefix" {
+  type = string
+}
+variable "app-nsg-sr2-source_port_range" {
+  type = string
+}
+/*//*/
+variable "app_subnet_id" {
+  type = string
+}
+variable "db-nsg-sr1-name" {
+  type =string  
+}
+variable "db-nsg-sr1-priority" {
+  type = string
+}
+variable "db-nsg-sr1-protocol" {
+  type = string
+}
+variable "db-nsg-sr1-access" {
+  type = string
+}
+variable "db-nsg-sr1-direction" {
+  type = string
+}
+variable "db-nsg-sr1-source_address_prefix" {
+  type = string
+}
+variable "db-nsg-sr1-source_port_range" {
+  type = string
+}
+variable "db-nsg-sr1-destination_address_prefix" {
+  type = string
+}
+variable "db-nsg-sr1-destination_port_range" {
+  type = string
+}/*//*/
+variable "db-nsg-sr2-name" {
+  type =string  
+}
+variable "db-nsg-sr2-priority" {
+  type = string
+}
+variable "db-nsg-sr2-protocol" {
+  type = string
+}
+variable "db-nsg-sr2-access" {
+  type = string
+}
+variable "db-nsg-sr2-direction" {
+  type = string
+}
+variable "db-nsg-sr2-source_address_prefix" {
+  type = string
+}
+variable "db-nsg-sr2-source_port_range" {
+  type = string
+}
+variable "db-nsg-sr2-destination_address_prefix" {
+  type = string
+}
+variable "db-nsg-sr2-destination_port_range" {
+  type = string
+}
