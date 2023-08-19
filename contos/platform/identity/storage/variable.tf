46a96dcc90771ab2aa0b3bacda2b7ac7b0dd5062
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-08-19 15:11:16 +0530

    Create variable.tf

---
+++ contos/platform/identity/storage/variable.tf
@@ -0,0 +1,18 @@
+variable "primary_database" {
+    type = string
+}
+variable "resource-group1-name" {
+    type = string
+}
+variable "location1" {
+    type = string
+}
+variable "primary_database_version" {
+    type = string
+}
+variable "primary_database_admin" {
+    type = string
+}
+variable "primary_database_password" {
+    type = string
+}
